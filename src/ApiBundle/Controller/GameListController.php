79ec7ba503e7201467a0527ba0c8c8cf275b5d42
Benjamin Garside <benjamin.garside@hpe.com>
2017-02-18 00:07:25 -0600

    added stuff

--- src/ApiBundle/Controller/GameListController.php
+++ src/ApiBundle/Controller/GameListController.php
@@ -16,19 +16,51 @@ class GameListController extends Controller
      *
      * @return JsonResponse
      */
-    public function indexAction($steamId)
+    public function gameListIndexAction($steamId)
     {
         $jsonResponse = new JsonResponse();
 
         /** @var SteamQueryService $steamQueryService */
         $steamQueryService = $this->get('steam_query_service');
 
-        $dataArray = $steamQueryService->querySteam();
+        $params = array(SteamQueryService::STEAM_ID => $steamId);
+        $dataArray = $steamQueryService->querySteam(SteamQueryService::GET_OWNED_GAMES, $params);
 
         $jsonResponse->setData($dataArray);
         $jsonResponse->setStatusCode(Response::HTTP_OK);
 
-        return $jsonResponse->send();
-        //return $this->render('ApiBundle:Default:index.html.twig');
+        return $jsonResponse;
+    }
+
+    /**
+     * @Route("/gamelist/unplayed/{steamId}")
+     * @param integer $steamId
+     *
+     * @return JsonResponse
+     */
+    public function gameListUnplayedGamesAction($steamId)
+    {
+        $jsonResponse = new JsonResponse();
+
+        /** @var SteamQueryService $steamQueryService */
+        $steamQueryService = $this->get('steam_query_service');
+
+        $params = array(SteamQueryService::STEAM_ID => $steamId);
+        $dataArray = $steamQueryService->querySteam(SteamQueryService::GET_OWNED_GAMES, $params);
+
+        $games = $dataArray["response"]["games"];
+        $unplayed = array();
+        foreach ($games as $game) {
+            if ($game["playtime_forever"] == 0) {
+                $unplayed[] = $game;
+            }
+        }
+
+        $dataArray["response"]["games"] = $unplayed;
+        
+        $jsonResponse->setData($dataArray);
+        $jsonResponse->setStatusCode(Response::HTTP_OK);
+
+        return $jsonResponse;
     }
 }
--- src/ApiBundle/Services/SteamQueryService.php
+++ src/ApiBundle/Services/SteamQueryService.php
@@ -11,6 +11,21 @@ class SteamQueryService
     //reference https://developer.valvesoftware.com/wiki/Steam_Web_API
     const GET_PLAYER_SUMMARIES = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/";
     const GET_OWNED_GAMES = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/";
+    const GET_GLOBAL_ACHIEVEMENT_PERCENTAGES_FOR_APP = "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/";
+
+    const STEAM_ID = "steamid";
+
+    /** @var string @apiKey */
+    private $apiKey;
+
+    /**
+     * SteamQueryService constructor.
+     * @param string $apiKey
+     */
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
 
     /**
      * @param   string  $url
@@ -19,14 +34,22 @@ class SteamQueryService
      */
     public function querySteam($url, $params)
     {
+        //get query url
         $queryUrl = $this->createQueryUrl($url, $params);
 
         //basic curl settings
-        $curlHandle = curl_init($queryUrl);
-        curl_setopt($curlHandle, CURLOPT_CUSTOMREQUEST, "GET");
-        //curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+        $curlHandle = curl_init();
+        curl_setopt($curlHandle, CURLOPT_URL, $queryUrl);
+        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curlHandle, CURLOPT_PROXY, '');
 
+        //execute query
         $result = curl_exec($curlHandle);
+
+        //close curl
+        curl_close($curlHandle);
+
+        //return result
         return json_decode($result, true);
     }
 
@@ -38,5 +61,19 @@ class SteamQueryService
      */
     private function createQueryUrl($url, $params) {
         //todo logic for applying params to url
+        $resultUrl = $url."?key=".$this->apiKey;
+        $i = 0;
+        foreach ($params as $key => $val) {
+            /*if ($i > 0) {
+                $resultUrl .= "&".$key."=".$val;
+            } else {
+                $resultUrl .= "?".$key."=".$val;
+            }*/
+
+            $resultUrl .= "&".$key."=".$val;
+        }
+        $resultUrl .= "&format=json";
+
+        return $resultUrl;
     }
 }
