af753cf13475866a5f6c6a6699edfad9f5ac2535
Benjamin Garside <benjamin.garside@hpe.com>
2017-03-21 12:15:46 -0500

    added skeleton for engine bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new ApiBundle\ApiBundle(),
+            new EngineBundle\EngineBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
